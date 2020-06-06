8c8f3eb19f21408c6e5a4d2bb770a41f2b768f05
Nabil Hayet <nabil.bracu@gmail.com>
2020-06-06 03:30:27 +0000

    Done.

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -14,7 +14,7 @@ class CreateCostumes < ActiveRecord::Migration[4.2]
       t.string    :name
       t.integer   :price
       t.string    :size
-      t.string    :imageurl
+      t.string    :image_url
       t.timestamp :created_at
       t.timestamp :updated_at
     end
