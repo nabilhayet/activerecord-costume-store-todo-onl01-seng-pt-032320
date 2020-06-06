aa5665ab8ee6d6b171d241e0f963d0a3aaecca9d
Nabil Hayet <nabil.bracu@gmail.com>
2020-06-06 03:12:05 +0000

    Done.

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,14 @@
 # Create your costume_stores migration here
+class CostumeStore < ActiveRecord::Migration[4.2]
+  def change
+    create_table  :costume_stores  do |t|
+      t.string    :name
+      t.string    :location
+      t.integer   :costume_inventory
+      t.integer   :num_of_employees
+      t.string    :still_in_business
+      t.timestamp :opening_time
+      t.timestamp :closing_time
+    end
+  end
+end
