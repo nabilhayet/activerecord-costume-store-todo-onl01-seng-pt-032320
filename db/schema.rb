35bdf759def2469b510ea9a8177942eab368e71e
Nabil Hayet <nabil.bracu@gmail.com>
2020-06-06 03:36:46 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,7 @@ ActiveRecord::Schema.define(version: 3) do
     t.string "location"
     t.integer "costume_inventory"
     t.integer "num_of_employees"
-    t.string "still_in_business"
+    t.boolean "still_in_business"
     t.datetime "opening_time"
     t.datetime "closing_time"
   end
@@ -26,7 +26,7 @@ ActiveRecord::Schema.define(version: 3) do
     t.string "name"
     t.integer "price"
     t.string "size"
-    t.string "imageurl"
+    t.string "image_url"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -36,10 +36,10 @@ ActiveRecord::Schema.define(version: 3) do
     t.string "location"
     t.string "theme"
     t.integer "price"
-    t.string "family_friendly"
+    t.boolean "family_friendly"
     t.datetime "opening_date"
     t.datetime "closing_date"
-    t.string "long_description"
+    t.string "description"
   end
 
 end
